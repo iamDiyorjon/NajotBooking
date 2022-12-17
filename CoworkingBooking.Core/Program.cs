1a4fa893eaa0841ded060821d8383dabe9ee7490
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-17 17:53:47 +0500

    INFRA: Create API Ptroject

--- CoworkingBooking.Core/Program.cs
+++ CoworkingBooking.Core/Program.cs
@@ -11,16 +11,14 @@ namespace CoworkingBooking.Core
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) =>
+             CreateHostBuilder(args).Build().Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.UseStartup<Startup>());
+        }
     }
 }
--- CoworkingBooking.Core/Startup.cs
+++ CoworkingBooking.Core/Startup.cs
@@ -16,44 +16,43 @@ namespace CoworkingBooking.Core
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration)=>
             Configuration = configuration;
-        }
+        
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            services.AddSwaggerGen(config =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoworkingBooking.Core", Version = "v1" });
+                config.SwaggerDoc(
+                    name: "v1",
+                    info: new OpenApiInfo { Title = "CoworkingBooking.Core", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoworkingBooking.Core v1"));
+
+                app.UseSwaggerUI(config => config.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json",
+                    name: "CoworkingBooking.Core v1"));
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+                endpoints.MapControllers());
         }
     }
 }
