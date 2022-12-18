1e50d79a21f38062253a1026c4e0dfc17c13291c
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-18 10:57:40 +0500

    DATA: Model And Migrate Users

---
+++ Coworking-Booking.Api/Brokers/Storages/StorageBroker.Users.cs
@@ -0,0 +1,10 @@
+﻿using Coworking_Booking.Api.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coworking_Booking.Api.Brokers.Storages
+{
+    public partial class StorageBroker : IStorageBroker
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
