ac92033ae803b16ca83be890e2d8ef05499a4773
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-18 12:26:43 +0500

    DATA: Model and Migrate Seats

---
+++ Coworking-Booking.Api/Brokers/Storages/StorageBroker.Seat.cs
@@ -0,0 +1,12 @@
+﻿using Coworking_Booking.Api.Models.Seats;
+using Coworking_Booking.Api.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Coworking_Booking.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Seat> Seats { get; set; }
+    }
+}
