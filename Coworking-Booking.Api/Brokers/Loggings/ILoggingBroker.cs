a6e2647d6cb0988ab57ca2372d4c57c4ed6c467f
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-18 14:39:23 +0500

    BROKERS: Add Logging

---
+++ Coworking-Booking.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Coworking_Booking.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
