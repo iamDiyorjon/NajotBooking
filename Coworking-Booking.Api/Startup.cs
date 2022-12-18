887418300ba521e4e16c9a834013b30fdd79e79c
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-18 15:28:54 +0500

    CONFIGS: Register Datetime Broker

--- Coworking-Booking.Api/Startup.cs
+++ Coworking-Booking.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Coworking_Booking.Api.Brokers.DateTimes;
 using Coworking_Booking.Api.Brokers.Loggings;
 using Coworking_Booking.Api.Brokers.Storages;
 using Microsoft.AspNetCore.Builder;
@@ -22,6 +23,7 @@ namespace Coworking_Booking.Api
             services.AddControllers();
             services.AddTransient<IStorageBroker, StorageBroker>();
             services.AddTransient<ILoggingBroker, LoggingBroker>();
+            services.AddTransient<IDateTimeBroker, DateTimeBroker>();
 
             services.AddSwaggerGen(config =>
             {
