b533a197fadb355bc6c6e5f4a127b39bb1cde3cc
Diyor <shumtakadiyor@gmail.com>
2022-12-18 16:04:53 +0500

    CODE RUB: Fix Service

--- Coworking-Booking.Api/Services/Foundations/Seats/SeatService.cs
+++ Coworking-Booking.Api/Services/Foundations/Seats/SeatService.cs
@@ -1,4 +1,6 @@
-﻿using Coworking_Booking.Api.Brokers.Storages;
+﻿using Coworking_Booking.Api.Brokers.DateTimes;
+using Coworking_Booking.Api.Brokers.Loggings;
+using Coworking_Booking.Api.Brokers.Storages;
 using Coworking_Booking.Api.Models.Seats;
 using System;
 using System.Linq;
@@ -9,20 +11,25 @@ namespace Coworking_Booking.Api.Services.Foundations.Seats
     public class SeatService : ISeatService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public SeatService(IStorageBroker storageBroker)
+        public SeatService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
         {
             this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
         }
 
         public ValueTask<Seat> AddSeatAsync(Seat seat) =>
             this.storageBroker.InsertSeatAsync(seat);
         
-
         public ValueTask<Seat> ModifySeatAsync(Seat seat) =>
             this.storageBroker.UpdateSeatAsync(seat);
         
-
         public ValueTask<Seat> RemoveSeat(Seat seat) =>
             this.storageBroker.DeleteSeatAsync(seat);
 
