87a58d1273923830ad813966af94caa51ca388e7
huseyntechs <xusannematovdes@gmail.com>
2023-01-25 23:54:05 +0500

    DATA: Order Model Created And Migrated

---
+++ Coworking-Booking.Api/Migrations/20230125185342_OrderTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoworkingBooking.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
