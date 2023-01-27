cdd14135055d6be21b8ff908b962027e16a99e29
Muhammad Muso <musogulomov123@gmail.com>
2023-01-27 17:44:33 +0500

    BROKERS: Delete Order

---
+++ Coworking-Booking.Api/Brokers/Storages/IStorageBroker.Order.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the THE STANDART SHARPISTS
+// Free To Use to Book Places in Coworking Zones
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Coworking_Booking.Api.Models.Orders;
+
+namespace Coworking_Booking.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Order> DeleteOrderAsync(Order order);
+    }
+}
---
+++ Coworking-Booking.Api/Brokers/Storages/StorageBroker.Order.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the THE STANDART SHARPISTS
+// Free To Use to Book Places in Coworking Zones
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Coworking_Booking.Api.Models.Orders;
+
+namespace Coworking_Booking.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public async ValueTask<Order> DeleteOrderAsync(Order order) =>
+            await DeleteAsync(order);
+    }
+}
