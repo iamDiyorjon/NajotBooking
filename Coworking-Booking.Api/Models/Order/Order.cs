ebf914d4c912097c228559c21177a3881fe83a0b
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-01-27 20:07:49 +0500

    CODE RUBE: Model Order

---
+++ Coworking-Booking.Api/Models/Order/Order.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition Of The THE STANDART SHARPISTS
+// Free To Use To Book Places In Coworking Zones
+// ---------------------------------------------------------------
+
+using Coworking_Booking.Api.Models.Seats;
+using Coworking_Booking.Api.Models.Users;
+using System;
+
+namespace Coworking_Booking.Api.Models.Order
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        
+        public Guid SeatId { get; set; }
+        public Seat Seat { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set;}
+        public 
+    }
+}
