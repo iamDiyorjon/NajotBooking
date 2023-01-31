f19ca747ad1e93b5bd16a366cdff1bed5396df9d
Muhammad Muso <musogulomov123@gmail.com>
2023-01-31 15:18:59 +0500

    CODE RUB: Refactored Seat Model

--- Coworking-Booking.Api/Models/Seats/Seat.cs
+++ Coworking-Booking.Api/Models/Seats/Seat.cs
@@ -13,8 +13,7 @@ namespace Coworking_Booking.Api.Models.Seats
         public int Number { get; set; }
         public Branch Branch { get; set; }
         public int Floor { get; set; }
-        public DateTimeOffset StartingTime { get; set; }
-        public DateTimeOffset EndingTime { get; set; }
+        public decimal Price { get; set; }
         public DateTimeOffset CraetedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
     }
