7b99daa08468a043f9f95cc6ea0157a2fe406d5c
Muhammad Muso <musogulomov123@gmail.com>
2023-02-01 15:15:32 +0500

    ShouldAddOrderAsync-> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the THE STANDART SHARPISTS
+// Free To Use to Book Places in Coworking Zones
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using NajotBooking.Api.Brokers.Storages;
+using NajotBooking.Api.Models.Orders;
+using NajotBooking.Api.Services.Foundations.Orders;
+using Tynamix.ObjectFiller;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Orders
+{
+    public partial class OrderServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IOrderService orderService;
+
+        public OrderServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.orderService = new OrderService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Order CreateRandomOrder() =>
+            CreateOrderFiller().Create();
+
+        private static Filler<Order> CreateOrderFiller()
+        {
+            var filler = new Filler<Order>();
+            DateTimeOffset dates = GetRandomDateTime();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
---
+++ NajotBooking.Api/Services/Foundations/Orders/OrderService.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the THE STANDART SHARPISTS
+// Free To Use to Book Places in Coworking Zones
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using NajotBooking.Api.Brokers.Storages;
+using NajotBooking.Api.Models.Orders;
+
+namespace NajotBooking.Api.Services.Foundations.Orders
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public OrderService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public ValueTask<Order> AddOrderAsync(Order order) =>
+            throw new NotImplementedException();
+    }
+}
