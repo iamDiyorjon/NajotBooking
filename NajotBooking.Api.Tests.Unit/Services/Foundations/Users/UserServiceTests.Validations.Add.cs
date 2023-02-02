b9a058b7400bcd7cd4928c2ff62460933a397c3f
GulchexrasGithub <burgulchexra@gmail.com>
2023-02-03 01:03:49 +0500

    ShouldThrowValidationExceptionOnAddIfUserIsNullAndLogItAsync -> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NajotBooking.Api.Models.Users;
+using NajotBooking.Api.Models.Users.Exceptions;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfUserIsNullAndLogItAsync()
+        {
+            // given
+            User nullUser = null;
+
+            var nullUserException =
+                new NullUserException();
+
+            var expecteUserValidationException =
+                new UserValidationException(nullUserException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(nullUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                    addUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expecteUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expecteUserValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
