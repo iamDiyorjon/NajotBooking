59e121d4a849f642e58d7de6e752eef197553e0a
GulchexrasGithub <burgulchexra@gmail.com>
2023-02-03 02:00:44 +0500

    ShouldThrowValidationExceptionOnAddIfUserIsInvalidAndLogItAsync -> FAIL

--- NajotBooking.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using NajotBooking.Api.Models.Users;
@@ -37,6 +38,68 @@ namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Users
                 broker.LogError(It.Is(SameExceptionAs(
                     expecteUserValidationException))),
                     Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfUserIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidGuid = Guid.Empty;
+
+            var invalidUser = new User
+            {
+                Id = invalidGuid
+            };
+
+            var invalidUserException =
+                new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+               key: nameof(User.LastName),
+               values: "Text is required");
+
+            invalidUserException.AddData(
+               key: nameof(User.FirstName),
+               values: "Text is required");
+
+            invalidUserException.AddData(
+               key: nameof(User.PhoneNumber),
+               values: "Text is required");
+
+            var expectedUserValidationException =
+                new UserValidationException(invalidUserException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(invalidUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                    addUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedUserValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(invalidUser),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))),
+                        Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
