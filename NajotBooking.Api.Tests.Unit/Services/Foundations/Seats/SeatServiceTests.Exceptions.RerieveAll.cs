afdce69dbcb5b3b24060493bf1b2bf474559bd4c
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-06 22:26:37 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt -> PASS

--- NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Exceptions.RerieveAll.cs
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Exceptions.RerieveAll.cs
@@ -10,34 +10,36 @@ namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
     public partial class SeatServiceTests
     {
         [Fact]
-        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt()
         {
-            // given
+            //given
             SqlException sqlException = CreateSqlException();
-            var failedSeatServiceException = new FailedSeatServiceException(sqlException);
+
+            var failedSeatStorageException =
+                new FailedSeatStorageException(sqlException);
 
             var expectedSeatDependencyException =
-                new SeatDependencyException(failedSeatServiceException);
+                new SeatDependencyException(failedSeatStorageException);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectAllSeats()).Throws(sqlException);
 
-            // when
-            Action retrieveAllSeatAction = () =>
+            //when
+            Action retrieveAllSeatsAction = () =>
                 this.seatService.RetrieveAllSeat();
 
             SeatDependencyException actualSeatDependencyException =
-                Assert.Throws<SeatDependencyException>(retrieveAllSeatAction);
+                Assert.Throws<SeatDependencyException>(retrieveAllSeatsAction);
 
-            // then
+            //then
             actualSeatDependencyException.Should().BeEquivalentTo(expectedSeatDependencyException);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectAllSeats(), Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogCritical(It.Is(SameExceptionAs(expectedSeatDependencyException))),
-                    Times.Once);
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedSeatDependencyException))), Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
