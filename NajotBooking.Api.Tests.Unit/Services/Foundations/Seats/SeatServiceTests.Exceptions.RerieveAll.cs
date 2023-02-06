43ad34c7347115906d50946a2130575d9aa4bf50
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-06 22:11:57 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Exceptions.RerieveAll.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using NajotBooking.Api.Models.Seats.Exceptions;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
+{
+    public partial class SeatServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+            var failedSeatServiceException = new FailedSeatServiceException(sqlException);
+
+            var expectedSeatDependencyException =
+                new SeatDependencyException(failedSeatServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllSeats()).Throws(sqlException);
+
+            // when
+            Action retrieveAllSeatAction = () =>
+                this.seatService.RetrieveAllSeat();
+
+            SeatDependencyException actualSeatDependencyException =
+                Assert.Throws<SeatDependencyException>(retrieveAllSeatAction);
+
+            // then
+            actualSeatDependencyException.Should().BeEquivalentTo(expectedSeatDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllSeats(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedSeatDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
