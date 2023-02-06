961338211816172c0b3988a828b1c156c8ab3df9
Muhammad Muso <musogulomov123@gmail.com>
2023-02-06 15:00:33 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync-> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition Of The THE STANDART SHARPISTS
+// Free To Use To Book Places In Coworking Zones
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NajotBooking.Api.Models.Orders;
+using NajotBooking.Api.Models.Orders.Exceptions;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Orders
+{
+    public partial class OrderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidOrderId = Guid.Empty;
+            var invalidOrderException = new InvalidOrderException();
+
+            invalidOrderException.AddData(
+                key: nameof(Order.Id),
+                values: "Id is required");
+
+            var expectedOrderValidationException =
+                new OrderValidationException(invalidOrderException);
+
+            // when
+            ValueTask<Order> removeOrderByIdTask =
+                this.orderService.RemoveOrderByIdAsync(invalidOrderId);
+
+            OrderValidationException actualOrderValidationException =
+                await Assert.ThrowsAsync<OrderValidationException>(removeOrderByIdTask.AsTask);
+
+            // then
+            actualOrderValidationException.Should().BeEquivalentTo(expectedOrderValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExpressionAs(
+                    expectedOrderValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteOrderAsync(It.IsAny<Order>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
