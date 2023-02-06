a0c99dfc0d1fec86e52127c384f72755ab1b9dd6
GulchexrasGithub <burgulchexra@gmail.com>
2023-02-06 22:25:22 +0500

    ShouldRetrieveUserByIdAsync -> PASS

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition Of The THE STANDART SHARPISTS
+// Free To Use To Book Places In Coworking Zones
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using NajotBooking.Api.Models.Users;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveUserByIdAsync()
+        {
+            // given
+            Guid randomUserId = Guid.NewGuid();
+            Guid inputUserId = randomUserId;
+            User randomUser = CreateRandomUser();
+            User storageUser = randomUser;
+            User expectedUser = storageUser.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(inputUserId))
+                    .ReturnsAsync(storageUser);
+
+            // when
+            User actualUser =
+                await this.userService.RetrieveUserByIdAsync(
+                    inputUserId);
+
+            // then
+            actualUser.Should().BeEquivalentTo(expectedUser);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(inputUserId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
