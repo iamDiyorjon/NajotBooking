2bf947c11e6d2704128efbae919cb14cea2e51e8
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-06 12:28:14 +0500

    ShouldAddSeatAsync -> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Logic.Add.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using NajotBooking.Api.Models.Seats;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
+{
+    public partial class SeatServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddSeatAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            Seat randomSeat = CreateRandomSeat(randomDateTime);
+            Seat inputSeat = randomSeat;
+            Seat persistedSeat = inputSeat;
+            Seat expectedSeat = persistedSeat.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Returns(randomDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertSeatAsync(inputSeat))
+                    .ReturnsAsync(persistedSeat);
+
+            // when
+            Seat actualSeat = await this.seatService.AddSeatAsync(inputSeat);
+
+            // then
+            actualSeat.Should().BeEquivalentTo(expectedSeat);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertSeatAsync(inputSeat), Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Moq;
+using NajotBooking.Api.Brokers.DateTimes;
+using NajotBooking.Api.Brokers.Loggings;
+using NajotBooking.Api.Brokers.Storages;
+using NajotBooking.Api.Models.Seats;
+using NajotBooking.Api.Services.Foundations.Seats;
+using Tynamix.ObjectFiller;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
+{
+    public partial class SeatServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly ISeatService seatService;
+
+        public SeatServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.seatService = new SeatService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Seat CreateRandomSeat(DateTimeOffset dates) =>
+            CreateSeatFiller(dates).Create();
+
+        private static Filler<Seat> CreateSeatFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Seat>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
