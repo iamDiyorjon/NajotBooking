e67732e371481a148363c4bcaa66d5ed0a816395
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-07 22:02:22 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Validations.RetrievById.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using NajotBooking.Api.Models.Seats.Exceptions;
+using NajotBooking.Api.Models.Seats;
+using FluentAssertions;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
+{
+    public partial class SeatServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            //given 
+            var invalidSeatId = Guid.Empty;
+
+            var invalidSeatException =
+                new InvalidSeatException();
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.Id),
+                values: "Id is required");
+
+            var expectedSeatValidationException = new
+                SeatValidationException(invalidSeatException);
+
+            //when 
+            ValueTask<Seat> retrieveSeatByIdTask =
+                this.seatService.RetrieveSeatByIdAsync(invalidSeatId);
+
+            SeatValidationException actualSeatValidationException =
+                await Assert.ThrowsAsync<SeatValidationException>(retrieveSeatByIdTask.AsTask);
+
+            //then
+            actualSeatValidationException.Should().BeEquivalentTo(expectedSeatValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSeatValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSeatByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
