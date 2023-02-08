7208f73c9fde00e123ade812cd348c1dfde08b58
Muhammad Muso <musogulomov123@gmail.com>
2023-02-08 08:55:23 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync-> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition Of The THE STANDART SHARPISTS
+// Free To Use To Book Places In Coworking Zones
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NajotBooking.Api.Models.Orders;
+using NajotBooking.Api.Models.Orders.Exceptions;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Orders
+{
+    public partial class OrderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidOrderId = Guid.Empty;
+            InvalidOrderException invalidOrderException = new InvalidOrderException();
+
+            invalidOrderException.AddData(
+                key: nameof(Order.Id),
+                values: "Id is required");
+
+            OrderValidationException expectedOrderValidationException =
+                new OrderValidationException(invalidOrderException);
+
+            // when
+            ValueTask<Order> retrieveOrderByIdTask =
+                this.orderService.RetrieveOrderByIdAsync(invalidOrderId);
+
+            OrderValidationException actaulOrderValidationException =
+                await Assert.ThrowsAsync<OrderValidationException>(
+                    retrieveOrderByIdTask.AsTask);
+
+            // then
+            actaulOrderValidationException.Should()
+                .BeEquivalentTo(expectedOrderValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOrderValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectOrderByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
