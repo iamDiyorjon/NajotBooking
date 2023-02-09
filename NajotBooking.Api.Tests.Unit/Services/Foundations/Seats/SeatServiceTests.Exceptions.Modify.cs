77bbb0593ab55722ff33a96bc485ad9f58d29ace
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-09 21:40:53 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Exceptions.Modify.cs
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Exceptions.Modify.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using NajotBooking.Api.Models.Seats;
 using NajotBooking.Api.Models.Seats.Exceptions;
@@ -57,6 +59,57 @@ namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            int minutesInPast = GetRandomNegativeNumber();
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            Seat randomSeat = CreateRandomSeat(randomDateTime);
+            Seat SomeSeat = randomSeat;
+            Guid seatId = SomeSeat.Id;
+            SomeSeat.CreatedDate = randomDateTime.AddMinutes(minutesInPast);
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedSeatException =
+                new FailedSeatStorageException(databaseUpdateException);
+
+            var expectedSeatDependencyException =
+                new SeatDependencyException(failedSeatException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSeatByIdAsync(seatId)).ThrowsAsync(databaseUpdateException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Returns(randomDateTime);
+
+            // when
+            ValueTask<Seat> modifySeatTask =
+                this.seatService.ModifySeatAsync(SomeSeat);
+
+            SeatDependencyException actualSeatDependencyException =
+              await Assert.ThrowsAsync<SeatDependencyException>(
+                  modifySeatTask.AsTask);
+
+            // then
+            actualSeatDependencyException.Should().BeEquivalentTo(
+                expectedSeatDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSeatByIdAsync(seatId), Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSeatDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
