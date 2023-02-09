e616109789dc9741472e26d610d337d13af0c2a0
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-09 22:32:32 +0500

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using NajotBooking.Api.Models.Seats;
+using NajotBooking.Api.Models.Seats.Exceptions;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
+{
+    public partial class SeatServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            //given
+            Guid someSeatId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedSeatStorageException =
+                new FailedSeatStorageException(sqlException);
+
+            var expectedSeatDependencyException =
+                new SeatDependencyException(failedSeatStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSeatByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Seat> deleteSeatAsync =
+                this.seatService.RemoveSeatByIdAsync(someSeatId);
+
+            SeatDependencyException actualSeatDependencyException =
+                await Assert.ThrowsAsync<SeatDependencyException>(deleteSeatAsync.AsTask);
+
+            //then
+            actualSeatDependencyException.Should().BeEquivalentTo(expectedSeatDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSeatByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedSeatDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
