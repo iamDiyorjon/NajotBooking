9d0375cf0edbb8b2dacfce5e3a8b3b672ebf67a2
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-09 22:24:11 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NajotBooking.Api.Models.Seats;
+using NajotBooking.Api.Models.Seats.Exceptions;
+using Xunit;
+
+namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
+{
+    public partial class SeatServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidSeatId = Guid.Empty;
+
+            var invalidSeatException =
+                new InvalidSeatException();
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.Id),
+                values: "Id is required");
+
+            var expectedSeatValidationException =
+                new SeatValidationException(invalidSeatException);
+
+            // when
+            ValueTask<Seat> removeSeatByIdTask =
+                this.seatService.RemoveSeatByIdAsync(invalidSeatId);
+
+            SeatValidationException actualSeatValidationException =
+                await Assert.ThrowsAsync<SeatValidationException>(removeSeatByIdTask.AsTask);
+
+            //then
+            actualSeatValidationException.Should().BeEquivalentTo(expectedSeatValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSeatValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteSeatAsync(It.IsAny<Seat>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
