1dc80de31697a4acb1ed2ec15e418d99a558a967
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-09 20:33:11 +0500

    ShouldThrowValidationExceptionOnModifyIfSeatIsInvalidAndLogItAsync -> FAIL

--- NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Validations.Modify.cs
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Seats/SeatServiceTests.Validations.Modify.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
@@ -37,6 +38,76 @@ namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Seats
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectSeatByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateSeatAsync(It.IsAny<Seat>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(null)]
+        [InlineData(default)]
+        public async Task ShouldThrowValidationExceptionOnModifyIfSeatIsInvalidAndLogItAsync(
+            int invalidNumber)
+        {
+            //given
+            var invalidSeat = new Seat
+            {
+                Number = invalidNumber
+            };
+
+            var invalidSeatException = new InvalidSeatException();
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.Id),
+                values: "Id is required");
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.Number),
+                values: "Number is required");
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.Floor),
+                values: "Number is required");
+
+            invalidSeatException.AddData(
+               key: nameof(Seat.Price),
+               values: "Price is required");
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.CreatedDate),
+                values: "Value is required");
+
+            invalidSeatException.AddData(
+                key: nameof(Seat.UpdatedDate),
+                "Value is required",
+                "Date is not recent",
+                $"Date is the same as {nameof(Seat.CreatedDate)}");
+
+            var expectedSeatValidationException =
+                new SeatValidationException(invalidSeatException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Returns(GetRandomDateTime);
+
+            //when
+            ValueTask<Seat> modifySeatTask = this.seatService.ModifySeatAsync(invalidSeat);
+
+            SeatValidationException actualSeatValidationException =
+                await Assert.ThrowsAsync<SeatValidationException>(modifySeatTask.AsTask);
+
+            //then
+            actualSeatValidationException.Should().BeEquivalentTo(expectedSeatValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedSeatValidationException))), Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateSeatAsync(It.IsAny<Seat>()), Times.Never);
