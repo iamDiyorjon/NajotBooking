8080b04e54f57021eb992ac90b37d8d3fae6ffeb
Muhammad Muso <musogulomov123@gmail.com>
2023-02-10 23:37:46 +0500

    ShouldThrowValidationExceptionOnModifyIfOrderIsInvalidAndLogItAsync-> FAIL

--- NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.Validations.Modify.cs
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.Validations.Modify.cs
@@ -45,5 +45,66 @@ namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Orders
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public async Task ShouldThrowValidationExceptionOnModifyIfOrderIsInvalidAndLogItAsync(
+            int invalidNumber)
+        {
+            // given
+            var invalidOrder = new Order
+            {
+                Duration = invalidNumber
+            };
+
+            var invalidOrderException = new InvalidOrderException();
+
+            invalidOrderException.AddData(
+                key: nameof(Order.SeatId),
+                values: "Id is required");
+
+            invalidOrderException.AddData(
+                key: nameof(Order.UserId),
+                values: "Id is required");
+
+            invalidOrderException.AddData(
+                key: nameof(Order.StartDate),
+
+                values: new string[] {
+                    "Value is required",
+                    $"Date is not less than {nameof(Order.EndDate)}"});
+
+            invalidOrderException.AddData(
+                key: nameof(Order.EndDate),
+                values: "Value is required");
+
+            invalidOrderException.AddData(
+                key: nameof(Order.Duration),
+                values: "Value is required");
+
+            OrderValidationException expectedOrderValidationException =
+                new OrderValidationException(invalidOrderException);
+
+            // when
+            ValueTask<Order> modifyOrderTask =
+                this.orderService.ModifyOrderAsync(invalidOrder);
+
+            OrderValidationException actualOrderValidationException =
+                await Assert.ThrowsAsync<OrderValidationException>(modifyOrderTask.AsTask);
+
+            // then
+            actualOrderValidationException.Should().BeEquivalentTo(expectedOrderValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExpressionAs(
+                    expectedOrderValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateOrderAsync(It.IsAny<Order>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
