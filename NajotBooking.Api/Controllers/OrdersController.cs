aff699a77ce817e7863bce515d785aad3e55869d
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-10 20:31:54 +0500

    EXPOSERS: Post Order

---
+++ NajotBooking.Api/Controllers/OrdersController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NajotBooking.Api.Models.Orders;
+using NajotBooking.Api.Models.Orders.Exceptions;
+using NajotBooking.Api.Services.Foundations.Orders;
+using RESTFulSense.Controllers;
+
+namespace NajotBooking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class OrdersController : RESTFulController
+    {
+        private readonly IOrderService orderService;
+
+        public OrdersController(IOrderService orderService) =>
+            this.orderService = orderService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Order>> PostOrderAsync(Order order)
+        {
+            try
+            {
+                return await this.orderService.AddOrderAsync(order);
+            }
+            catch (OrderValidationException orderValidationExpection)
+            {
+                return BadRequest(orderValidationExpection.InnerException);
+            }
+            catch (OrderDependencyValidationException orderDependencyValidationException)
+                when (orderDependencyValidationException.InnerException is AlreadyExistsOrderException)
+            {
+                return Conflict(orderDependencyValidationException.InnerException);
+            }
+            catch (OrderDependencyValidationException orderDependencyValidationException)
+            {
+                return BadRequest(orderDependencyValidationException.InnerException);
+            }
+            catch (OrderDependencyException orderDependencyException)
+            {
+                return InternalServerError(orderDependencyException.InnerException);
+            }
+            catch (OrderServiceException orderServiceException)
+            {
+                return InternalServerError(orderServiceException);
+            }
+        }
+    }
+}
