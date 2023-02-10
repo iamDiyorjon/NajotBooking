798f8d6e5f1bc2f55018abf8ea56d6b4aa7f153c
Muhammad Muso <musogulomov123@gmail.com>
2023-02-11 01:49:29 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync-> FAIL

--- NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.Exceptions.Modify.cs
+++ NajotBooking.Api.Tests.Unit/Services/Foundations/Orders/OrderServiceTests.Exceptions.Modify.cs
@@ -7,6 +7,7 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using NajotBooking.Api.Models.Orders;
 using NajotBooking.Api.Models.Orders.Exceptions;
@@ -58,6 +59,50 @@ namespace NajotBooking.Api.Tests.Unit.Services.Foundations.Orders
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateOrderAsync(someOrder), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            Order randomOrder = CreateRandomOrder();
+            randomOrder.EndDate = GetAfterRandomDateTime(randomOrder.StartDate);
+            randomOrder.Duration = GetRandomNumber();
+            Order someOrder = randomOrder;
+            Guid orderId = someOrder.Id;
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedOrderStorageException =
+                new FailedOrderStorageException(databaseUpdateException);
+
+            var expectedOrderDependencyException =
+                new OrderDependencyException(failedOrderStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectOrderByIdAsync(orderId))
+                    .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<Order> modifyOrderTask =
+                this.orderService.ModifyOrderAsync(someOrder);
+
+            OrderDependencyException actualOrderDependencyException =
+                await Assert.ThrowsAsync<OrderDependencyException>(
+                     modifyOrderTask.AsTask);
+
+            // then
+            actualOrderDependencyException.Should().BeEquivalentTo(
+                expectedOrderDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectOrderByIdAsync(orderId), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOrderDependencyException))), Times.Once);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
